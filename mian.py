24426f517b8ecef1a21b7da4a1a5d28956745f13
parsa98 <parsaassadi@aut.ac.ir>
2018-10-03 16:24:16 +0330

    Exhaustive search impelemented(Not GUI)

--- mian.py
+++ mian.py
@@ -8,13 +8,17 @@ matplotlib.use("QT5Agg")
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as f
 from matplotlib.figure import Figure
 import numpy as np
+from itertools import permutations
+
+
 
 
 class Point:
-    def __init__(self, X, Y, visited):
+    def __init__(self, X, Y, visited, nme):
         self.x = X
         self.y = Y
         self.isVisited = visited
+        self.name = nme
 
 form = uic.loadUiType(os.path.join(os.getcwd(),"TSPUi.ui"))[0]
 class intro(form, QMainWindow):
@@ -30,11 +34,47 @@ class intro(form, QMainWindow):
 
         self.numberOfCities = 0
         self.cities = []
+        self.firstCityToVisit = "None"
 
         self.addFile.clicked.connect(self.addFileCalled)
 
     def distanceOfTwoPoint(self, point1, point2):
         return np.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
+
+    def cummulativeSumOfDistancesOfPermutation(self, perms):
+        differentDistancesMadeByEachPermutation = []
+        currentSum = 0
+        counter = 0
+        for permutation in list(perms):
+            for element in permutation:
+                if counter == len(permutation) - 1:
+                    break
+                else:
+                    currentSum = currentSum + self.distanceOfTwoPoint(element, permutation[counter + 1])
+                    counter = counter + 1
+            differentDistancesMadeByEachPermutation.append(currentSum)
+            currentSum = 0
+            counter = 0
+        return differentDistancesMadeByEachPermutation
+
+
+    def exhaustiveSearch(self, set):
+        allPermutations = permutations(set,len(set))
+        allPossiblePermutations = []
+        for permutation in list(allPermutations):
+            if permutation[0].name == 'P1':
+                allPossiblePermutations.append(permutation)
+            else:
+                continue
+
+
+        for permutation in list(allPossiblePermutations):
+            for element in permutation:
+                print(element.name)
+            print('-------------')
+        print(self.cummulativeSumOfDistancesOfPermutation(allPossiblePermutations))
+
+
 
 
     def addFileCalled(self):
@@ -51,15 +91,15 @@ class intro(form, QMainWindow):
                     if counter == 0:
                         self.numberOfCities = int(element)
                     else:
-                        self.cities.append(Point(float(element[0]),float(element[2]), False))
+                        splitedElement = element.split(' ')
+                        self.cities.append(Point(float(splitedElement[0]),float(splitedElement[1]), False, 'P'+str(counter)))
                     counter = counter + 1
                 print('Contents Of File:')
                 print(self.numberOfCities)
                 for element in self.cities:
                     print(element.x, element.y)
-
-
-
+                self.firstCityToVisit = self.cities[0]
+                self.exhaustiveSearch(self.cities)
 
 
 
