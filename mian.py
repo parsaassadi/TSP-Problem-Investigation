2edfc94efef6f332433a800dd99a4ec9e1f9dba0
parsa98 <parsaassadi@aut.ac.ir>
2018-10-04 10:33:21 +0330

    Exhaustive search linked to GUI

--- mian.py
+++ mian.py
@@ -9,6 +9,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as f
 from matplotlib.figure import Figure
 import numpy as np
 from itertools import permutations
+import time
 
 
 
@@ -20,7 +21,9 @@ class Point:
         self.isVisited = visited
         self.name = nme
 
+
 form = uic.loadUiType(os.path.join(os.getcwd(),"TSPUi.ui"))[0]
+
 class intro(form, QMainWindow):
     def __init__(self):
         form.__init__(self)
@@ -32,11 +35,23 @@ class intro(form, QMainWindow):
         I = QVBoxLayout(self.plot)
         I.addWidget(self.canvas)
 
+
+
         self.numberOfCities = 0
         self.cities = []
         self.firstCityToVisit = "None"
+        self.exhaustiveSearchRuntime = 'None'
 
         self.addFile.clicked.connect(self.addFileCalled)
+        self.execute.clicked.connect(self.executeCalled)
+
+    def executeCalled(self):
+        if self.es.isChecked():
+            self.exhaustiveSearch(self.cities)
+        elif self.nn.isChecked():
+            print('linear')
+        else:
+            print('Choose an alg!')
 
     def distanceOfTwoPoint(self, point1, point2):
         return np.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
@@ -48,7 +63,7 @@ class intro(form, QMainWindow):
         for permutation in list(perms):
             for element in permutation:
                 if counter == len(permutation) - 1:
-                    break
+                    currentSum = currentSum + self.distanceOfTwoPoint(element, permutation[0])
                 else:
                     currentSum = currentSum + self.distanceOfTwoPoint(element, permutation[counter + 1])
                     counter = counter + 1
@@ -57,8 +72,20 @@ class intro(form, QMainWindow):
             counter = 0
         return differentDistancesMadeByEachPermutation
 
+    def plotAGivenPermutation(self, perm):
+        cnt = 0
+        for point in perm:
+            if cnt == len(perm) - 1:
+                self.ax.plot([point.x, perm[0].x], [point.y, perm[0].y], 'bo-')
+            else:
+                self.ax.plot([point.x,perm[cnt + 1].x], [point.y,perm[cnt + 1].y], 'ro-')
+            cnt = cnt + 1
+
+        self.canvas.draw()
 
     def exhaustiveSearch(self, set):
+        print('Exhaustive Search:')
+        t_start = time.clock()
         allPermutations = permutations(set,len(set))
         allPossiblePermutations = []
         for permutation in list(allPermutations):
@@ -67,12 +94,35 @@ class intro(form, QMainWindow):
             else:
                 continue
 
+        minimumDistanceAmongPermutations = min(self.cummulativeSumOfDistancesOfPermutation(allPossiblePermutations))
+        IndexOfMinimimDistanceAmongPermutations = self.cummulativeSumOfDistancesOfPermutation(allPossiblePermutations)\
+            .index(minimumDistanceAmongPermutations)
+        t_end = time.clock()
+        self.exhaustiveSearchRuntime = t_end - t_start
 
-        for permutation in list(allPossiblePermutations):
-            for element in permutation:
-                print(element.name)
-            print('-------------')
-        print(self.cummulativeSumOfDistancesOfPermutation(allPossiblePermutations))
+        #############################################################
+        ### UNCOMMMENT THESE LINES FOR DISPLAYNG ALL PERMUTATIONS ###
+        #############################################################
+        #cnt = 0
+        #for permutation in list(allPossiblePermutations):
+        #    for element in permutation:
+        #        print(element.name,end ='-')
+        #    print(': ',self.cummulativeSumOfDistancesOfPermutation(allPossiblePermutations)[cnt])
+        #    cnt = cnt + 1
+
+        print('------------------------------')
+        print('Optimum Path:')
+        for index in range(0,self.numberOfCities):
+            print(allPossiblePermutations[IndexOfMinimimDistanceAmongPermutations][index].name,end = '-')
+        print(': ', minimumDistanceAmongPermutations)
+        print('Runtime: ', self.exhaustiveSearchRuntime)
+
+        listOfBestPermutation = list(allPossiblePermutations[IndexOfMinimimDistanceAmongPermutations])
+        self.plotAGivenPermutation(listOfBestPermutation)
+
+
+
+
 
 
 
@@ -94,12 +144,10 @@ class intro(form, QMainWindow):
                         splitedElement = element.split(' ')
                         self.cities.append(Point(float(splitedElement[0]),float(splitedElement[1]), False, 'P'+str(counter)))
                     counter = counter + 1
-                print('Contents Of File:')
-                print(self.numberOfCities)
-                for element in self.cities:
-                    print(element.x, element.y)
+
                 self.firstCityToVisit = self.cities[0]
-                self.exhaustiveSearch(self.cities)
+
+        self.exhaustiveSearch(self.cities)
 
 
 
